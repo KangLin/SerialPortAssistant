f7a8e1dd3915870566285966405ec6aad4c1ff79
KangLin <kl222@126.com>
2018-06-19 16:54:31 +0800

    Modify ci

--- ci/qt-installer.sh
+++ ci/qt-installer.sh
@@ -2,11 +2,14 @@
 
 #http://stackoverflow.com/questions/25105269/silent-install-qt-run-installer-on-ubuntu-server
 #http://doc.qt.io/qtinstallerframework/noninteractive.html
+#参考：https://github.com/benlau/qtci
+#     https://github.com/mjscosta/qt-silent-installer
+
 set -e #quit on error
 
 if [ $# -lt 2 ];
 then
-    echo qt-installer.sh qt-installer output_path
+    echo qt-installer.sh qt-installer-file output_path
     exit -1
 fi
 
@@ -15,16 +18,36 @@ export WORKDIR=$PWD
 INSTALLER=$1
 OUTPUT=$2
 SCRIPT="$(mktemp /tmp/tmp.XXXXXXXXX)"
+case $RABBIT_ARCH in
+    arm*)
+        SELECTEDPACKAGES=android_armv7
+        ;;
+     x86)
+        SELECTEDPACKAGES=android_x86
+        ;;
+     unix)
+        SELECTEDPACKAGES=gcc_64
+        ;;
+       *)
+       echo "Aach[$RABBIT_ARCH] don't suppoert"
+esac
 
 cat <<EOF > $SCRIPT
 function Controller() {
+    installer.autoRejectMessageBoxes();
     installer.installationFinished.connect(function() {
         gui.clickButton(buttons.NextButton);
     });
 }
 
+function log() {
+    var msg = ["QTCI: "].concat([].slice.call(arguments));
+
+    console.log(msg.join(" "));
+}
+
 Controller.prototype.WelcomePageCallback = function() {
-    gui.clickButton(buttons.NextButton);
+    gui.clickButton(buttons.NextButton, 3000);
 }
 
 Controller.prototype.CredentialsPageCallback = function() {
@@ -32,9 +55,33 @@ Controller.prototype.CredentialsPageCallback = function() {
 }
 
 Controller.prototype.ComponentSelectionPageCallback = function() {
+    var components = installer.components();
+    log("Available components: " + components.length);
+    for (var i = 0 ; i < components.length ;i++) {
+        log(components[i].name);
+    }
+    log("Select components");
+    function trim(str) {
+        return str.replace(/^ +/,"").replace(/ *$/,"");
+    }
     var widget = gui.currentPageWidget();
-
-    // widget.deselectAll();
+    widget.deselectAll();
+    var packages = trim("$SELECTEDPACKAGES").split(",");
+    if (packages.length > 0 && packages[0] !== "") {
+        
+        for (var i in packages) {
+            var pkg = trim(packages[i]);
+            for (var i = 0 ; i < components.length ;i++) {
+                if(components[i].name.indexOf(pkg) != -1)
+                {
+                    log("Select " + components[i].name);
+                    widget.selectComponent(trim(components[i].name));
+                }
+            }
+        }
+    } else {
+        log("Use default component list");
+    }
 
     gui.clickButton(buttons.NextButton);
 }
@@ -81,4 +128,7 @@ Controller.prototype.FinishedPageCallback = function() {
 EOF
 
 chmod u+x $1
-$1 --script $SCRIPT
+#显示log
+$1 -v --script $SCRIPT
+#不显示log
+#$1 --script $SCRIPT
