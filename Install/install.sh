a28de33b526cb5b4910cef689b624ef0b569e134
KangLin <kl222@126.com>
2019-06-26 10:20:05 +0800

    Modify install.sh

--- Install/install.sh
+++ Install/install.sh
@@ -11,17 +11,19 @@ case "$1" in
     
     install|*)
         echo "install ..."
-        if [ ! -f /usr/share/applications/SerialPortAssistant.desktop ]; then
-            ln -s `pwd`/share/applications/SerialPortAssistant.desktop /usr/share/applications/SerialPortAssistant.desktop
-            sed -i "s/Exec=.*//g" /usr/share/applications/SerialPortAssistant.desktop
-            echo "Exec=`pwd`/SerialPortAssistant-x86_64.AppImage" >> /usr/share/applications/SerialPortAssistant.desktop
+        if [ -f /usr/share/applications/SerialPortAssistant.desktop ]; then
+            rm -f /usr/share/applications/SerialPortAssistant.desktop
         fi
-        if [ ! -f /usr/share/pixmaps/SerialPortAssistant.png ]; then
-            if [ ! -d /usr/share/pixmaps ]; then
-                mkdir -p /usr/share/pixmaps
-            fi
-            ln -s `pwd`/share/pixmaps/SerialPortAssistant.png /usr/share/pixmaps/SerialPortAssistant.png
-        fi
-        ;;
+        ln -s `pwd`/share/applications/SerialPortAssistant.desktop /usr/share/applications/SerialPortAssistant.desktop
+        sed -i "s/Exec=.*//g" `pwd`/share/applications/SerialPortAssistant.desktop
+        echo "Exec=`pwd`/SerialPortAssistant-x86_64.AppImage" >> `pwd`/share/applications/SerialPortAssistant.desktop
         
+        if [ -f /usr/share/pixmaps/SerialPortAssistant.png ]; then
+            rm -f /usr/share/pixmaps/SerialPortAssistant.png
+        fi
+        if [ ! -d /usr/share/pixmaps ]; then
+            mkdir -p /usr/share/pixmaps
+        fi
+        ln -s `pwd`/share/pixmaps/SerialPortAssistant.png /usr/share/pixmaps/SerialPortAssistant.png
+        ;;
 esac
